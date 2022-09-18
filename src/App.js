a9680c8255d4835568a456e3b5828aa740e20abf
Itay Cohen <itaycs2407@gmail.com>
2022-09-18 21:36:12 +0300

    inital design

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 /*global chrome*/
 import React, { useState, useEffect, useMemo } from "react";
+import styled from "styled-components";
 import Groups from "./components/groups";
 import "./App.css";
 
 const App = () => {
   const [groups, setGroups] = useState(null);
-  const [selectedGroupIndex, setSelectedGroupIndex] = useState(-1);
+  const [selectedGroupIndex, setSelectedGroupIndex] = useState([]);
+
+  const handelAddRemove = (index) => {
+    const isInArray =
+      selectedGroupIndex.findIndex((idx) => idx === index) !== -1;
+    isInArray
+      ? setSelectedGroupIndex((prev) => [
+          ...prev.filter((item) => item !== index),
+        ])
+      : setSelectedGroupIndex((prev) => [...prev, index]);
+  };
 
   const getGroups = () =>
     chrome.runtime.sendMessage({ type: "getGroups" }, (res) => {
@@ -13,15 +24,18 @@ const App = () => {
     });
 
   const handleConvert = () => {
-    chrome.runtime.sendMessage(
-      {
-        type: "converGroup",
-        groupTitle: groups[selectedGroupIndex].title,
-        tabs: groups[selectedGroupIndex].tabsInCurrentGroup,
-      },
-      () => {}
-    );
-    setSelectedGroupIndex(-1);
+    selectedGroupIndex.forEach((groupIndex) => {
+      chrome.runtime.sendMessage(
+        {
+          type: "converGroup",
+          groupTitle: groups[groupIndex].title,
+          tabs: groups[groupIndex].tabsInCurrentGroup,
+        },
+        () => {}
+      );
+    });
+
+    setSelectedGroupIndex([]);
   };
 
   useEffect(() => {
@@ -29,22 +43,76 @@ const App = () => {
   }, []);
 
   return (
-    <div className="App">
-      <Groups
-        groups={groups}
-        selectedGroup={selectedGroupIndex}
-        setSelectedGroup={setSelectedGroupIndex}
-      />
-      {selectedGroupIndex !== -1 && (
+    <Container>
+      {groups !== null ? (
         <>
-          <div>{`The selcted group is ${groups[selectedGroupIndex].title}`}</div>
+          <div>Select group to convert</div>
+          <Groups
+            groups={groups}
+            selectedGroup={selectedGroupIndex}
+            setSelectedGroup={handelAddRemove}
+          />
         </>
+      ) : (
+        <div>No groups found</div>
       )}
-      <button onClick={handleConvert} disabled={selectedGroupIndex === -1}>
+
+      <ConvertButton
+        onClick={handleConvert}
+        disabled={selectedGroupIndex.length === 0}
+      >
         Convert
-      </button>
-    </div>
+      </ConvertButton>
+    </Container>
   );
 };
 
 export default App;
+
+const Container = styled.div`
+  background-image: linear-gradient(315deg, #f8b500 0%, #fceabb 75%);
+  padding: 20px 50px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  box-shadow: inset 2px 2px 2px 0px rgba(255, 255, 255, 0.5);
+`;
+
+const ConvertButton = styled.button`
+  margin-top: 50px;
+  min-width: 130px;
+  height: 40px;
+  color: #fff;
+  padding: 5px 10px;
+  font-weight: bold;
+  cursor: pointer;
+  transition: all 0.3s ease;
+  position: relative;
+  display: inline-block;
+  outline: none;
+  border-radius: 5px;
+  z-index: 0;
+  background: #fff;
+  overflow: hidden;
+  border: 2px solid #495057;
+  color: #495057;
+
+  &:hover {
+    color: #fff;
+  }
+  &:hover:after {
+    height: 100%;
+  }
+  &:after {
+    content: "";
+    position: absolute;
+    z-index: -1;
+    transition: all 0.3s ease;
+    left: 0;
+    top: 0;
+    height: 0;
+    width: 100%;
+    background: #495057;
+  }
+`;
--- src/components/groups.js
+++ src/components/groups.js
@@ -1,16 +1,59 @@
 import React from "react";
+import styled from "styled-components";
 
 const Groups = ({ groups, selectedGroup, setSelectedGroup }) => {
   return (
-    <ul>
+    <Ul>
       {groups !== null &&
         groups.map((group, index) => (
-          <li key={index} onClick={() => setSelectedGroup(index)}>
+          <Li
+            key={index}
+            onClick={() => setSelectedGroup(index)}
+            selected={selectedGroup.includes(index)}
+          >
             {group.title}
-          </li>
+          </Li>
         ))}
-    </ul>
+    </Ul>
   );
 };
 
 export default Groups;
+
+const Ul = styled.ul`
+  padding: 0;
+  gap: 10px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Li = styled.li`
+  min-width: 130px;
+  height: 40px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  color: #fff;
+  text-align: center;
+  font-weight: bold;
+  cursor: pointer;
+  transition: all 0.3s ease;
+  position: relative;
+  outline: none;
+
+  border-radius: 20px;
+  border: none;
+  box-shadow: inset 2px 2px 2px 0px rgba(255, 255, 255, 0.5),
+    7px 7px 20px 0px rgba(0, 0, 0, 0.1), 4px 4px 5px 0px rgba(0, 0, 0, 0.1);
+  background: ${(props) => (props.selected ? "#80ed99" : "#57cc99")};
+
+  :hover {
+    background-color: #80ed99;
+  }
+  :active {
+    top: 2px;
+  }
+`;
